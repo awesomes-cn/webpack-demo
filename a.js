7c23c9c6d8e969c42a2aed17a9c7b081575734f4
黄小红 <hxh@localhost.(none)>
2016-03-09 23:19:04 -0800

    feat(): Use commit message rule

---
+++ a.js
@@ -0,0 +1,2 @@
+require(['./b.js','./c.js'],function(){
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 var webpack = require("webpack")
 
 module.exports = {
-  entry: "./entry.js",
+  entry:  {
+    a: "./a.js"
+  },
   output: {
-    path: __dirname,
-    filename: "bundle.js"
+    path: __dirname + "/dist",
+    filename: "[name].js"
   },
   module: {
     loaders: [
       {test: /\.css$/,loader: "style!css!"}
     ]
-  }
+  },
+  resolve: {
+    root: [
+      __dirname + "/dist",
+    ]
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({maxChunks: 1})
+  ]
+ 
 }
